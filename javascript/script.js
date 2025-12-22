1b373474edf894eb31b57575ba57843454d88543
SteveMan67 <poppt@acatulsa.org>
2025-12-22 17:23:00 -0600

    fixed js and added the OpenHistoricalMap style to it

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,19 @@
 var map = new maplibregl.Map({
   container: 'map',
-  style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+  style: 'https://www.openhistoricalmap.org/map-styles/main/main.json', // stylesheet location
   center: [-74.5, 40], // starting position [lng, lat]
   zoom: 9 // starting zoom
-  
+});
+
+map.addControl(new maplibregl.NavigationControl());
+
+map.on('load', () => {
+  // Hide modern roads
+  map.setLayoutProperty('road', 'visibility', 'none');
+
+  // Hide buildings
+  map.setLayoutProperty('building', 'visibility', 'none');
+
+  // Hide POIs
+  map.setLayoutProperty('poi', 'visibility', 'none');
+});
